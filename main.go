308120b99d07bc8986241cfb23168b17e4c75cb7
David Poulsen <dapoman@gmail.com>
2022-08-29 21:36:52 +0200

    Showing off using HTTP server

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"webapp/i18n"
 )
 
 func main() {
 
-	i18n.SetLanguage("nl")
 	if err := i18n.Load("locale"); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Translation of Title:", i18n.Translate("general.app_title"))
+	http.HandleFunc("/nl", func(w http.ResponseWriter, r *http.Request) {
+		i18n.SetLanguage("nl")
+		fmt.Println("Translation of Title:", i18n.Translate("general.app_title"))
+		fmt.Fprintf(w, "Title %s", i18n.Translate("general.app_title"))
+	})
+	http.HandleFunc("/en", func(w http.ResponseWriter, r *http.Request) {
+		i18n.SetLanguage("en")
+		fmt.Println("Translation of Title:", i18n.Translate("general.app_title"))
+		fmt.Fprintf(w, "Title %s", i18n.Translate("general.app_title"))
+	})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
